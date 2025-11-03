fef0392131cfa04dc3b586ca7f9f25c7b2d697d1
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-11-03 16:51:53 +0100

    Crea rotte in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetail"
 
 function App() {
 
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/movie/:id" element={<MovieDetail/>} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   )
 }
 
