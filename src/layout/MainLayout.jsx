8b10e15400be46dd993b25b1081eeafa57297c1d
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-11-03 16:58:17 +0100

    Creazione MainLayout con import di Outlet

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,4 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar"
 
-export default function MainLayout
+export default function MainLayout() {
+    return (
+        <>
+            <Navbar />
+            <main className="container">
+                <Outlet/>
+            </main>
+        </>
+    )
+}
