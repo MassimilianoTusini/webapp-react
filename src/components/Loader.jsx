eb4db758d8389827de13e8a1257b911a0644e44c
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-11-06 15:59:24 +0100

    Crea component loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return(
+        <div className="orlay">
+            <div className="spinner-border text-warning" role="status"></div>
+        </div>
+    )
+}
